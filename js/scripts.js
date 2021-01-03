2deab5c41aa39b6e18fee7542a70d5d815f3b791
michaelcruzcervera <michaelcruz1998@hotmail.com>
2021-01-03 15:09:43 +0100

    V0.6

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,128 @@
 
 $(function(){
 
-    main()
-})
+    bust();
+    main();
 
-function main() {
+});
+
+function main(){
+  var full = $(".workBx");
+
+  //Loops over all elements that have the class fullscreen
+
+  $('a.with-transition').click(function (e) {
+    e.preventDefault();                   // prevent default anchor behavior
+    var goTo = this.getAttribute("href"); // store anchor href
+    full.each(function(index, elem) {
+      $(elem).click(fullscreenClick);
+    });
+    setTimeout(function(){
+         window.location = goTo;
+    }, 500);                             // time in ms
+});
+
+  window.addEventListener('scroll', function(){
+    toggleSticky();
+  });
+
+  $(window).on("load",function() {
+    function fade() {
+      var animation_height = $(window).innerHeight() * 0.1;
+      var ratio = Math.round( (1 / animation_height) * 10000 ) / 1000;
+
+      $('section div').each(function() {
+
+        var objectTop = $(this).offset().top;
+        var objectBottom = $(this).position().top + ($(this).outerHeight());
+        var windowTop = $(window).scrollTop();
+        var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+        if ( objectTop < windowBottom && objectBottom > windowTop) {
+          $(this).css( {
+            opacity: 1,
+            animation: 'animate_in 0.2s normal ease-out'
+          } );
+        } else {
+          $(this).css( {opacity: 0, animation: 'unset' });
+
+        }
+      });
+    }
+    $('.fade').css( 'opacity', 0 );
+    fade();
+    $(window).scroll(function() {fade();});
+    $(window).resize(function() {fade();});
+  });
+
+}
+
+function fullscreenClick() {
+    //The button is this
+    //We want to clone the parent
+    var box = $(this);
+    // /var image = $(this).child();
+    //create a holder box so the layout stays the same
+    var holder = $(this).clone(false, true);
+    //and make it not visible
+    $(holder).css({
+      "visibility": "hidden"
+    });
+
+    //Get its position
+    var pos = $(box).position();
+
+    //Substitute our box with our holder
+    $(box).before($(holder));
+
+    //Set the position of our box (not holder)
+    //Give it absolute position (eg. outside our set structure)
+    $(box).css({
+      "position": "fixed",
+      "z-index": "100000",
+      "left": pos.left + "px",
+      "top": pos.top + "px",
+      "border-radius": "0",
+      "padding": 0,
+      "object-fit": "none",
+      "background-position": "center center",
+      "background-repeat": "no-repeat"
+    });
+    /*
+    $(image).css({
+      "width":"100%",
+      "height":"auto",
+      "object-fit": "none",
+      "background-position": "center center",
+      "background-repeat": "no-repeat"
+    });*/
+
+    //Set class so it can be animated
+    $(box).addClass("fullscreen");
+
+    //Animate the position
+    $(box).animate({
+      "top": 0,
+      "left": 0,
+    }, 500);
+
+  }
+
+function toggleSticky(){
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+}
+
+function toggleMenu(){
+  var menu = document.querySelector('.menu');
+  var hamburger = document.querySelector('.hamburger')
+  hamburger.classList.toggle("is-active");
+  menu.classList.toggle('active');
+}
+
+
+
+function bust() {
   const canvas = document.querySelector('#bust');
   const mouseArea = document.querySelector('#about');
   const renderer = new THREE.WebGLRenderer({canvas,  alpha: true});
@@ -47,7 +165,6 @@ function main() {
     console.error( error );
 
   } );
-
   this.mouse = new THREE.Vector2(0, 0)
   mouseArea.addEventListener('mousemove', (ev) => { this.onMouseMove(ev, mesh) })
   mouseArea.addEventListener('mouseleave', (ev) => { this.onMouseLeave(ev, mesh) })
@@ -55,8 +172,10 @@ function main() {
 
   requestAnimationFrame(render);
 
-  function render() {
-    TWEEN.update();
+  function render(time) {
+
+    TWEEN.update(time);
+
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -80,6 +199,7 @@ function onMouseMove(event, cube) {
   var tween = new TWEEN.Tween(cube.rotation)
                   .to({x:-this.mouse.y * 0.3 ,
                     y: this.mouse.x * (Math.PI / 6)}, 300)
+                  .easing(TWEEN.Easing.Quadratic.Out)
                   .start();
 }
 
@@ -87,6 +207,7 @@ function onMouseLeave(event, cube) {
   var tween = new TWEEN.Tween(cube.rotation)
                   .to({x:0,
                     y: 0}, 600)
+                  .easing(TWEEN.Easing.Quadratic.Out)
                   .start();
 }
 
